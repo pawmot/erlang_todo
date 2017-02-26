0fbe7ad7923ba04124a3e59153993f674e93e9b9
Paweł Motyl <pawmot@gmail.com>
2017-02-26 15:25:03 +0100

    Fix for Erlang 'after' limit; Interface for 'event' module.

--- src/event.erl
+++ src/event.erl
@@ -2,16 +2,53 @@
 -author("pawmot").
 
 %% API
--export([loop/1]).
+-export([start/2, start_link/2, cancel/1]).
 
 -record(state, {server,
   name = "",
-  to_go = 0}).
+  to_go = []}).
 
-loop(S = #state{server = Server}) ->
+start(EventName, Delay) ->
+  Server = self(),
+  spawn(fun() -> init(Server, EventName, Delay) end).
+
+start_link(EventName, Delay) ->
+  Server = self(),
+  spawn_link(fun() -> init(Server, EventName, Delay) end).
+
+cancel(Pid) ->
+  %% Monitor in case the process is already dead/
+  Ref = erlang:monitor(process, Pid),
+  Pid ! {self(), Ref, cancel},
+  receive
+    {Ref, ok} ->
+      erlang:demonitor(Ref, [flush]),
+      ok;
+    {'DOWN', Ref, process, Pid, _Reason} ->
+      ok
+  end.
+
+%%% event's innards
+init(Server, EventName, Delay) ->
+  loop(#state{
+    server = Server,
+    name = EventName,
+    to_go = normalize(Delay)}).
+
+loop(S = #state{server = Server, to_go = [T | Next]}) ->
   receive
     {Server, Ref, cancel} ->
       Server ! {Ref, ok}
-  after S#state.to_go * 1000 ->
-    Server ! {done, S#state.name}
+  after T * 1000 ->
+    if Next =:= [] ->
+      Server ! {done, S#state.name};
+      Next =/= [] ->
+        loop(S#state{to_go = Next})
+    end
   end.
+
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used.
+normalize(N) ->
+  Limit = 49 * 24 * 60 * 60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
