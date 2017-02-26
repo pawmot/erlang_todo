684267c1579ce59790d3883caa7c0a48205e03b5
Paweł Motyl <pawmot@gmail.com>
2017-02-26 15:56:50 +0100

    Handle adding new event

--- src/evserv.erl
+++ src/evserv.erl
@@ -22,7 +22,16 @@ loop(S = #state{}) ->
       Pid ! {MsgRef, ok},
       loop(S#state{clients = NewClients});
     {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
-      ok;
+      case valid_datetime(TimeOut) of
+        true ->
+          EventPid = event:start_link(Name, TimeOut),
+          NewEvents = orddict:store(Name, #event{name = Name, description = Description, pid = EventPid, timeout = TimeOut}, S#state.events),
+          Pid ! {MsgRef, ok},
+          loop(S#state{events = NewEvents});
+        false ->
+          Pid ! {MsgRef, {error, bad_timeout}},
+          loop(S)
+      end;
     {Pid, MsgRef, {cancel, Name}} ->
       ok;
     {done, Name} ->
@@ -37,3 +46,20 @@ loop(S = #state{}) ->
       io:format("Unknown message: ~p~n", [Unknown]),
       loop(S)
   end.
+
+valid_datetime({Date, Time}) ->
+  try
+    calendar:valid_date(Date) andalso valid_time(Time)
+  catch
+    error:function_clause ->
+      false
+  end;
+valid_datetime(_) ->
+  false.
+
+valid_time({H, M, S}) -> valid_time(H, M, S).
+valid_time(H, M, S) when
+  H >= 0, H < 24,
+  M >= 0, M < 60,
+  S >= 0, S < 60 -> true;
+valid_time(_, _, _) -> false.
